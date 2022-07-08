391792d0eedfc60c682f6886418d8407240f7f98
Manideep1308 <manideep.sankabathula@innoart.in>
2022-07-08 16:32:22 +0530

    few changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
-from flask import Flask, request, Response, json
+from flask import Flask, request, Response, json, render_template
 from flask_cors import CORS
-
+import os
 
 # import fileinput
 
@@ -211,6 +211,136 @@ def repeat():
      '   "changingto" : "'+str(afterreplace)+'"\n'
      '}\n'
    ) 
+
+
+
+
+@app.route('/index', methods=['POST', 'GET'])
+def fun1():
+
+ composefilename=request.json['composefilename']
+
+ f= open(str(composefilename), 'r')
+ list=f.readlines() 
+ return render_template("table.html", len =len(list), list=list)    
+
+
+
+@app.route('/appendvolumeusingindex', methods=['POST'])
+def volappen():
+
+ composefilename=request.json['composefilename']
+ volumename =request.json['volumename']                           #body params
+ path =request.json['path']
+ index = request.json['index']  
+
+ data2 =("      - "+(volumename) +":/"+path+"\n") 
+
+ with open(str(composefilename), 'r') as f:
+    contents = f.readlines()
+ contents.insert(int(index), data2)
+ with open(str(composefilename), "w") as f:
+    contents = "".join(contents)
+    f.write(contents) 
+ return  (
+     '{\n'
+     '   "'+str(volumename)+'" : "appended" \n'
+     '}\n'
+   ) 
+
+
+
+@app.route('/appendenvusingindex', methods=['POST'])
+def env():
+
+ composefilename=request.json['composefilename']
+ username =request.json['username']                  #body params
+ password =request.json['password']
+ dbserver =request.json['dbserver'] 
+ index = request.json['index'] 
+
+
+ data1=("    environment:\n"
+        "      ADMINUSERNAME: "+(username)+"\n"
+        "      ADMINPASSWORD: "+(password)+"\n"
+        "      SERVER: "+(dbserver)+"\n") 
+
+
+
+     
+
+ with open(str(composefilename), 'r') as f:
+     contents = f.readlines()
+ contents.insert(int(index), data1)
+ with open(str(composefilename), "w") as f:
+    contents = "".join(contents)
+    f.write(contents) 
+ return  (
+     '{\n'
+     '   "'+str(dbserver)+'" : "appended" \n'
+     '}\n'
+   ) 
+
+
+
+
+
+@app.route('/delete', methods =['POST']) 
+def test():
+
+ composefilename=request.json['composefilename']
+ index=request.json['index']
+
+ with open(str(composefilename), "r+") as f:
+    lines = f.readlines()
+    del lines[int(index)]  # use linenum - 1 if linenum starts from 1
+    f.seek(0)
+    f.truncate()
+    f.writelines(lines)
+
+ return(
+    '{\n'
+     '   "'+str(index)+'" : "deleted" \n'
+    '}\n'
+   ) 
+    
+
+@app.route('/deletemultiple', methods=['POST'])
+def delete_multiple_lines():
+    
+    filename=request.json['filename']
+    index=request.json['index']
+
+    is_skipped = False
+    counter = 0
+    original_file=filename
+    line_numbers=index
+        
+    dummy_file = original_file + '.bak'
+    with open(original_file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+     
+        for line in read_obj:
+           
+            if counter not in line_numbers:
+                write_obj.write(line)
+            else:
+                is_skipped = True
+            counter += 1
+   
+    if is_skipped:
+        os.remove(original_file)
+        os.rename(dummy_file, original_file)
+    else:
+        os.remove(dummy_file)
+
+
+    return  (
+     '{\n'
+     '   "'+str(index)+' lines" : "deleted" \n'
+     '}\n'
+   ) 
+
+
 
 @app.errorhandler(Exception)
 def all_exception_handler(error):
